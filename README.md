0019b037933c3d4730733a81cadb9760be1eea2d
Supereg <mail@anderl-bauer.de>
2020-07-04 00:01:57 +0200

    Adding system information to the output

--- README.md
+++ README.md
@@ -13,7 +13,24 @@ mdns-diagnostics
 # output should look like this
 
 ```
-mdns-diagnostics
+$ mdns-diagnostics
+--------------------
+Platform: darwin
+Version: Darwin Kernel Version 19.5.0: Tue May 26 20:41:44 PDT 2020; root:xnu-6153.121.2~2/RELEASE_X86_64
+Network information:
+{
+  "lo0": [
+    "ipv4: 127.0.0.1/8",
+    "routableIpv6: ::1/128",
+    "ipv6: fe80::1/64"
+  ],
+  "en0": [
+    "ipv6: fe80::1ff:fe23:4567:890a/64",
+    "ipv4: 192.168.1.34/24",
+    "routableIpv6: 2001:db8:85a3::8a2e:370:7334/64"
+  ]
+}
+--------------------
 Leonard-Dev E1BD, Leonard-Dev,  192.168.1.51:51828
 Howard-Hue 6E78, Howard-Hue,  192.168.1.51:51827
 Howard 56F2, Howard,  192.168.1.71:51826
@@ -27,4 +44,5 @@ Walter-NodeRed-Channel-A188, Walter-NodeRed-Channel,  192.168.1.72:51840
 sheldon-megan 1692, sheldon-megan,  192.168.1.51:51833
 Leonard-NodeRed-1C5E, Leonard-NodeRed,  192.168.1.71:51831
 Leonard-Amazon 035D, Leonard-Amazon,  192.168.1.51:51829
+--------------------
 ```
--- mdns-diagnostics.js
+++ mdns-diagnostics.js
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
 
+const os = require("os");
+
+console.log("--------------------");
+console.log("Platform: " + os.platform());
+console.log("Version: " + os.version());
+console.log("Network information:");
+
+const ifaces = os.networkInterfaces();
+for (const [name, infos] of Object.entries(ifaces)) {
+  ifaces[name] = infos.map(info => {
+    let type = "uIp";
+    if (info.family === "IPv4") {
+      type = "ipv4";
+    } else if (info.family === "IPv6") {
+      if (info.scopeid) {
+        type = "ipv6";
+      } else if (info.scopeid === 0) {
+        type = "routableIpv6";
+      }
+    }
+
+    return `${type}: ${info.cidr}`;
+  });
+}
+
+console.log(JSON.stringify(ifaces, null, 2));
+
+console.log("--------------------");
+
 var bonjour = require('bonjour-hap')();
 
 var browser = bonjour.find({
@@ -16,5 +45,6 @@ var browser = bonjour.find({
 setTimeout(function() {
   // debug('Timeout:');
   browser.stop();
+  console.log("--------------------");
   process.exit(0);
 }, 20 * 1000);
